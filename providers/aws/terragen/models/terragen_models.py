b03ac883945caa7cad63cb6a5a488e259d053da6
hunt3ri <iain.hunter@tescobank.com>
2021-09-30 00:24:04 +0100

    wip

--- providers/aws/terragen/models/terragen_models.py
+++ providers/aws/terragen/models/terragen_models.py
@@ -1,4 +1,39 @@
+import textwrap
+
 import attr
+
+
+@attr.s
+class TerraformAWSProvider:
+    region: str = attr.ib()
+    profile: str = attr.ib()
+
+    def __str__(self):
+        return textwrap.dedent(f"""
+            provider "aws" {{
+                region  = "{self.region}"  
+                profile = "{self.profile}"
+            }}   
+            """)
+
+
+@attr.s
+class TerraformS3Backend:
+    bucket: str = attr.ib()
+    region: str = attr.ib()
+    profile: str = attr.ib()
+    key: str = attr.ib()
+
+    def __str__(self):
+        return textwrap.dedent(f"""
+            backend "s3" {{
+                profile = "{self.profile}"
+                region  = "{self.region}"
+                bucket  = "{self.bucket}" 
+                key     = "{self.key}"
+            }}
+            """
+        )
 
 
 @attr.s
@@ -6,6 +41,8 @@ class TerragenProperties:
     run_terraform: bool = attr.ib()
     terraform_mode: str = attr.ib()
     terraform_init_cmd: str = attr.ib()
+    provider = TerraformAWSProvider = attr.ib()
+    backend = TerraformS3Backend = attr.ib()
 
     @terraform_mode.validator
     def validate_terraform_mode(self, attribute, value):
---
+++ providers/aws/terragen/tests/models/test_terragen_models.py
@@ -0,0 +1,16 @@
+from providers.aws.terragen.models.terragen_models import TerraformS3Backend, TerraformAWSProvider
+
+
+def test_terraform_s3_backend():
+    s3_backend = TerraformS3Backend(bucket="test-bucket",
+                                    region="us-east-1",
+                                    profile="hunter-labs",
+                                    key="/shared/test")
+
+    print(s3_backend)
+
+
+def test_terraform_aws_provider():
+    aws_provider = TerraformAWSProvider(region="us-east-1",
+                                        profile="hunter-labs")
+    print(aws_provider)
