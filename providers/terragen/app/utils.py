cd6e8cd139d7de3590fcb57422a3e8b9c667c2f2
hunt3ri <iain.hunter@tescobank.com>
2021-11-02 23:27:02 +0000

    bugfix

--- providers/terragen/app/utils.py
+++ providers/terragen/app/utils.py
@@ -1,9 +1,13 @@
 import toml
 
 
-def to_toml(key, value):
+def to_toml(key: str, value):
     """ Helper function returns the supplied key/value as valid toml """
-    return toml.dumps({key: value}).strip()
+    if isinstance(value, str) and (value.startswith("data.")):
+        return f"{key} = {value}"  # Lookups are variables and shouldn't be quoted
+    else:
+        return toml.dumps({key: value}).strip()
+
 
 
 def get_lookup_values(lookup: str):
--- providers/terragen/tests/app/test_utils.py
+++ providers/terragen/tests/app/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from providers.terragen.app.utils import get_lookup_values
+from providers.terragen.app.utils import get_lookup_values, to_toml
 
 
 def test_get_lookup_values():
@@ -11,3 +11,12 @@ def test_get_lookup_values():
 def test_get_lookup_values_raises_error_for_missing_outputs():
     with pytest.raises(ValueError):
         get_lookup_values("lookup: shared.vpc.simple_vpc")
+
+
+def test_to_toml_happy_path():
+    assert to_toml("happy", "path") == 'happy = "path"'
+
+
+def test_to_toml_handles_lookups():
+    lookup_value = to_toml("lookup", "data.terraform_remote_state.local_access.outputs.vpc_id")
+    assert lookup_value == "lookup = data.terraform_remote_state.local_access.outputs.vpc_id"
