ffab4a24a326a868b46f059c30e5c05d88a33755
hunt3ri <iain.hunter@tescobank.com>
2021-11-17 23:33:42 +0000

    tidy up modules

--- modules/aws/ec2/main.tf
+++ modules/aws/ec2/main.tf
@@ -1,5 +1,5 @@
 
-module "sandbox_sg" {
+module "this_sg" {
   # SG registry module https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest
   source               = "terraform-aws-modules/security-group/aws"
   version              = "~> 4.0"
@@ -14,15 +14,15 @@ module "sandbox_sg" {
   egress_rules         = ["all-all"]
 }
 
-resource "aws_instance" "sandbox" {
-  # Create EC2 Sandbox Instance(s) using base ubuntu AMI
+resource "aws_instance" "this_instance" {
+  # Create EC2 instance(s)
   count                       = var.instance_count
   ami                         = data.aws_ami.hunter_labs_sandbox.id
   instance_type               = var.instance_type
 
   subnet_id                   = data.terraform_remote_state.vpc.outputs.public_subnet_ids[count.index]
   associate_public_ip_address = var.associate_public_ip_address
-  vpc_security_group_ids      = [module.sandbox_sg.security_group_id]
+  vpc_security_group_ids      = [module.this_sg.security_group_id]
 
   key_name                    = data.terraform_remote_state.key_pair.outputs.key_name
 
@@ -32,12 +32,12 @@ resource "aws_instance" "sandbox" {
   }
 }
 
-resource "aws_route53_record" "sandbox_dns_record"{
-  # Create a DNS record for the sandbox server so we can refer to it by name rather than IP address
+resource "aws_route53_record" "dns_record"{
+  # Create a DNS record for the EC2 instance
   count   = var.instance_count
   zone_id = data.aws_route53_zone.hunter_labs_zone.id
   name    = "${var.instance_name}${count.index + 1}-${var.environment}.${data.aws_route53_zone.hunter_labs_zone.name}"
   type    = "A"
   ttl     = "60"
-  records = [aws_instance.sandbox[count.index].public_ip]
+  records = [aws_instance.this_instance[count.index].public_ip]
 }
