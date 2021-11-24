aeaa737e62086cd66f5a81c074f44b8c43fe5453
hunt3ri <iain.hunter@tescobank.com>
2021-11-24 22:30:44 +0000

    test

--- terragen/cli.py
+++ terragen/cli.py
@@ -8,7 +8,7 @@ from terragen.providers.cloud_provider import CloudProvider
 log = logging.getLogger(__name__)
 
 
-@hydra.main(config_path="config", config_name="config")
+@hydra.main(config_path="../config", config_name="config", config_dir="test")
 def entrypoint(cfg: DictConfig) -> None:
     """Parse config and create or destroy infrastructure"""
     log.info("Terragen starting up")
