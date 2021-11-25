bca9b82cf936a8bbbbf515fd628ad2b9422f1efc
hunt3ri <iain.hunter@tescobank.com>
2021-11-25 17:37:38 +0000

    0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = "https://github.com/hunt3ri/terragen"
 EMAIL = "iain.hunter@outlook.com"
 AUTHOR = "Iain Hunter"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.0.24"
+VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
