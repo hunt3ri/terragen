3071475035577e434a4bca29dfd42f34ef1cee18
hunt3ri <iain.hunter@tescobank.com>
2022-02-13 17:00:34 +0000

    add EOT parse

--- terragen/providers/aws/app/utils.py
+++ terragen/providers/aws/app/utils.py
@@ -12,6 +12,8 @@ def to_toml(key: str, value):
     """Helper function returns the supplied key/value as valid toml"""
     if isinstance(value, str) and (value.startswith("data.")):
         return f"{key} = {get_env_var(value)}"  # Lookups are variables and shouldn't be quoted
+    elif isinstance(value, str) and value.startswith("<<EOT"):
+        return f"{key} = {value}"
     elif isinstance(value, DictConfig):
         return to_hcl_map(key, value)
     else:
